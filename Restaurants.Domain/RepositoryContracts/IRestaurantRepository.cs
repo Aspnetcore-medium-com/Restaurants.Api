52c2404e418e61a7edf70592accf2ba29d16cf53
raja <arajavignesh@gmail.com>
2026-03-16 17:39:45 +0000

    changes

---
+++ Restaurants.Domain/RepositoryContracts/IRestaurantRepository.cs
@@ -0,0 +1,14 @@
+﻿using Restaurants.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restaurants.Domain.RepositoryContracts
+{
+    public interface IRestaurantRepository
+    {
+        Task<IReadOnlyList<Restaurant>> GetRestaurants();
+    }
+}
---
+++ Restaurants.Infrastructure/Repositories/RestaurantRepository.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Restaurants.Domain.Entities;
+using Restaurants.Domain.RepositoryContracts;
+using Restaurants.Infrastructure.Persistance;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restaurants.Infrastructure.Repositories
+{
+    public class RestaurantRepository : IRestaurantRepository
+    {
+        private readonly ApplicationDBContext _dbContext;
+        public RestaurantRepository(ApplicationDBContext applicationDBContext) { 
+            _dbContext = applicationDBContext;
+        }
+        public async Task<IReadOnlyList<Restaurant>> GetRestaurants()
+        {
+            IReadOnlyList<Restaurant> restaurants =  await _dbContext.Restaurants.AsNoTracking().ToListAsync();
+            return restaurants;
+        }
+    }
+}
